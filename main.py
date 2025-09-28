15b33100db661b432951813892b1283d900fbf83
asi_nasti <fohelanastasiia@gmail.com>
2025-09-28 13:28:32 +0000

    add right/left rotate

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+
+
 def main():
     pygame.init()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    dt = 0
+
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+    
